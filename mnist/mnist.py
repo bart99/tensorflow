8e995a51d4828a53be8cc5c6692ec70a9e0f43d5
Sungho Park <bart0724@gmail.com>
2017-03-05 22:46:52 +0900

    insert tensorflow sample code

---
+++ mnist/mnist.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+# data set loading
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+# set up model
+x = tf.placeholder(tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+
+# cost function
+
+# y = tf.nn.softmax(tf.matmul(x, W) + b)
+# y_ = tf.placeholder(tf.float32, [None, 10])
+# cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+y = tf.matmul(x, W) + b
+y_ = tf.placeholder(tf.float32, [None, 10])
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+
+# optimization algorithm
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+
+# initialize
+init = tf.initialize_all_variables()
+
+sess = tf.Session()
+sess.run(init)
+
+# learning
+for i in range(1000):
+  batch_xs, batch_ys = mnist.train.next_batch(100)
+  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+
+# validation
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
